3df65447b2807b615132f949ea0e3a6ce44a1801
MrCrashLab <m.dudarev2002@mail.ru>
2020-09-19 12:57:52 +0300

    Commit

---
+++ src/Practic_8/Eigth_1/WindowApp.java
@@ -0,0 +1,20 @@
+package Practic_8.Eigth_1;
+
+import javax.swing.*;
+
+public class WindowApp extends JFrame {
+    public WindowApp() {
+        setTitle("Фигуры");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocation(300, 200);
+        JLayeredPane lp = getLayeredPane();
+        setResizable(false);
+        setSize(500, 500);
+        setVisible(true);
+        add(new DrawPanel());
+    }
+
+    public static void main(String[] args){
+        new WindowApp();
+    }
+}
---
+++ src/Practic_8/Eigth_3/MainWindow.java
@@ -0,0 +1,21 @@
+package Practic_8.Eigth_3;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame{
+    public MainWindow(){
+        setTitle("Анимация");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(500,500);
+        setLocation(200, 200);
+        setResizable(false);
+        add(new AppField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new MainWindow();
+    }
+}
+
+
