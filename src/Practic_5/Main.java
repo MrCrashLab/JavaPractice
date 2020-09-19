3d1c421e0bd07b396a2ece5b7c21e87966c4adf8
MrCrashLab <m.dudarev2002@mail.ru>
2020-09-19 13:11:40 +0300

    Commit

---
+++ src/Practic_5/Main.java
@@ -0,0 +1,11 @@
+package Practic_5;
+
+public class Main {
+
+    public static void main(String[] args) {
+		MovableCircle movableCircle = new MovableCircle(5,6,23,12, 25);
+		MovablePoint movablePoint = new MovablePoint(3, 3,4,1);
+		System.out.println(movableCircle.toString());
+		System.out.println(movablePoint.toString());
+    }
+}
