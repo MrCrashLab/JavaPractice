7e9b5eba525f339fa047b1c3c7de6a92b6659996
MrCrashLab <m.dudarev2002@mail.ru>
2020-09-20 22:54:11 +0300

    Commit

---
+++ src/Practic_11/Num_3/AppPanel.java
@@ -0,0 +1,19 @@
+package Practic_11.Num_3;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AppPanel extends JPanel {
+    private JTextArea textArea = new JTextArea("");
+
+    public AppPanel() {
+        setLayout(new BorderLayout());
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        add(new JScrollPane(textArea), BorderLayout.CENTER);
+        textArea.setFont(new Font("Times New Roman", Font.BOLD, 20));
+    }
+
+    public void changeColor(Color color){textArea.setForeground(color);}
+    public void changeFont(Font font){textArea.setFont(font);}
+}
