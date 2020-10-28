85eecd1fc864b8536e0e59c419cc839e7b4c410f
MrCrashLab <m.dudarev2002@mail.ru>
2020-10-28 13:49:24 +0300

    Commit

--- src/Practic_15_16/Imaginarium.java
+++ src/Practic_15_16/Imaginarium.java
@@ -4,41 +4,42 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Imaginarium {
-    private ArrayList<Node> nodeList = new ArrayList<>();
-    private HashMap<Integer, String> transitionAction = new HashMap<>();
+    private final ArrayList<Node> nodeList = new ArrayList<>();
+    private final HashMap<Integer, String> transitionAction = new HashMap<>();
 
     public Imaginarium() {
         createTransitAction();
         createNodeList();
     }
 
-    public void job(ArrayList<Integer> transition){
+    public void job(ArrayList<Integer> transition) {
         int startNode = 0;
         for (int i = 0; i < transition.size(); i++) {
             System.out.println(transitionAction.get(nodeList.get(startNode).getActionMap().get(transition.get(i))));
             startNode = nodeList.get(startNode).getTransitionMap().get(transition.get(i));
         }
     }
-    private void createNodeList(){
-        Node tmp = new Node(0, 1, 0, 1,4,1);
+
+    private void createNodeList() {
+        Node tmp = new Node(0, 1, 0, 1, 4, 1);
         nodeList.add(tmp);
-        tmp = new Node(0,2,3,1,3,5);
+        tmp = new Node(0, 2, 3, 1, 3, 5);
         nodeList.add(tmp);
-        tmp = new Node(0,3,5,1,4,1);
+        tmp = new Node(0, 3, 5, 1, 4, 1);
         nodeList.add(tmp);
-        tmp = new Node(0,2,2,1,4,4);
+        tmp = new Node(0, 2, 2, 1, 4, 4);
         nodeList.add(tmp);
-        tmp = new Node(0,0,4,1,2,2);
+        tmp = new Node(0, 0, 4, 1, 2, 2);
         nodeList.add(tmp);
     }
 
-    private void createTransitAction(){
-        transitionAction.put(0,"create_project");
-        transitionAction.put(1,"add_library");
-        transitionAction.put(2,"restart");
-        transitionAction.put(3,"test");
-        transitionAction.put(4,"deploy");
-        transitionAction.put(5,"drop_db");
+    private void createTransitAction() {
+        transitionAction.put(0, "create_project");
+        transitionAction.put(1, "add_library");
+        transitionAction.put(2, "restart");
+        transitionAction.put(3, "test");
+        transitionAction.put(4, "deploy");
+        transitionAction.put(5, "drop_db");
     }
 
 
--- src/Practic_15_16/Node.java
+++ src/Practic_15_16/Node.java
@@ -3,13 +3,14 @@ package Practic_15_16;
 import java.util.HashMap;
 
 public class Node {
-    private HashMap<Integer, Integer> transitionMap = new HashMap<>();
-    private HashMap<Integer, Integer> actionMap = new HashMap<>();
+    private final HashMap<Integer, Integer> transitionMap = new HashMap<>();
+    private final HashMap<Integer, Integer> actionMap = new HashMap<>();
+
     public Node(int trans0, int node0, int action0, int trans1, int node1, int action1) {
         transitionMap.put(trans0, node0);
         transitionMap.put(trans1, node1);
-        actionMap.put(trans0,action0);
-        actionMap.put(trans1,action1);
+        actionMap.put(trans0, action0);
+        actionMap.put(trans1, action1);
     }
 
     public HashMap<Integer, Integer> getTransitionMap() {
