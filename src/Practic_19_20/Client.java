4776c9f7b4510c7d4497fd09a1a29e2d150f067e
MrCrashLab <m.dudarev2002@mail.ru>
2020-11-14 20:40:11 +0300

    Commit

--- src/Practic_19_20/Client.java
+++ src/Practic_19_20/Client.java
@@ -5,7 +5,7 @@ import java.net.*;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         try {
             DatagramSocket socket = new DatagramSocket(248);
             System.out.print("Enter your name: ");
